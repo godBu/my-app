1ab33886e6c5dec41e40bfb565f418671a8f6699
Ebuka Mbata <rockymmpr@gmail.com>
2020-05-18 14:31:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p id="paragraph">Bubu is a good boy</p>
+    </div>
+  );
+}*/
+const AboutMe = () => {
+  return (
+    <div>
+    <h1>hello</h1>
+    <p>I rock</p>
+    </div>
+  )
+}
+
+const AboutUs = () => {
+  return (
+    <div>
+    <h1>ola</h1>
+    <p>We rock</p>
+    </div>
+  )
+}
+
+const AboutThem = () => {
+  return (
+    <div>
+    <h1>yo</h1>
+    <p>They defo suck</p>
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Hello World</h1>
+      <AboutMe />
+      <AboutUs />
+      <AboutThem />
+      </div>
+    )
+  }
+}
+
+export default App;
+
+/*const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Bubu is a good boy</p>
+    </div>
+  );
+}
+
+export default App;*/
+
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +86,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
